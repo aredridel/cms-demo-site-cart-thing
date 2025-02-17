2bf62caea6eef6481913d56c45347be2b9b746d2
Aria Stewart <aredridel@dinhe.net>
2025-02-16 23:26:14 -0500

    Remove items from the cart after

--- _cart/checkout-download.svelte
+++ _cart/checkout-download.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+    import { items } from "./cart.svelte.ts";
     const params = new URLSearchParams(location.search);
     const key = params.get("s");
-    const items = JSON.parse(atob(params.get("d")));
+    const downloads = JSON.parse(atob(params.get("d")));
 
-    console.log(items);
+    for (const [sku,] of downloads) {
+        const idx = items.findIndex(i => i.sku == sku);
+        if (idx >= 0) {
+            items.splice(idx, 1);
+        }
+    }
 </script>
 
 <svelte:options customElement="checkout-download" />
@@ -11,7 +17,7 @@
 <p>Your downloads: </p>
 
 <ul>
-    {#each items as item}
+    {#each downloads as item}
         {@const [sku, name] = item}
         <li><a href="checkout.phar.php?d={sku}&key={key}" download>{name}</a></li>
     {/each}
